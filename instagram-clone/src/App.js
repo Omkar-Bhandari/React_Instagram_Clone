a3d996b2441d6280db75dbd200a7f79378b1efa5
Omkar-Bhandari <68837275+Omkar-Bhandari@users.noreply.github.com>
2020-12-10 19:40:27 +0530

    initial

---
+++ instagram-clone/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <img className="app_logo" src="https://www.vectorlogo.zone/logos/instagram/instagram-wordmark.svg"
+          alt="Instagram-text-logo"/>
+      </div>
+
+      <h1>Hello This is Omkar</h1>
+
+
+    </div>
+  );
+}
+
+export default App;
